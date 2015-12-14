6170ca8c0e3a98627f180e640e5691b138da4ef7
Tommy Ettinger <tommy.ettinger@gmail.com>
2015-12-14 02:19:06 -0800

    The Tsar will have your head! Initial demo ready.

---
+++ TheTsar/android/src/com/squidpony/the/tsar/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.squidpony.the.tsar.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.squidpony.the.tsar.TsarGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new TsarGame(), config);
+	}
+}
---
+++ TheTsar/desktop/src/com/squidpony/the/tsar/desktop/DesktopLauncher.java
@@ -0,0 +1,18 @@
+package com.squidpony.the.tsar.desktop;
+
+import com.badlogic.gdx.Files;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.squidpony.the.tsar.TsarGame;
+
+public class DesktopLauncher {
+	public static void main (String[] args) {
+    	LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+   		config.width = 120 * 8;
+    	config.height = 40 * 18;
+    	config.addIcon("Tentacle-16.png", Files.FileType.Classpath);
+    	config.addIcon("Tentacle-32.png", Files.FileType.Classpath);
+    	config.addIcon("Tentacle-128.png", Files.FileType.Classpath);
+    	new LwjglApplication(new TsarGame(), config);
+    }
+}
