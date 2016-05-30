a0583df774ea84e3be4d9c24216912ee41debd7b
Tommy Ettinger <tommy.ettinger@gmail.com>
2016-05-30 13:06:48 -0700

    Early version of showcase demo
    Also a not-yet-working version of a Kotlin translation of the basic
    demo. The showcase includes distance field icons, with colors applied as
    if they are text. It also fixes some subtle issues with EverythingDemo
    from the main SquidLib source.

---
+++ SquidLibKotlinDemo/gwt/src/main/java/com/github/SquidPony/gwt/GwtLauncher.java
@@ -0,0 +1,20 @@
+package com.github.SquidPony.gwt;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.github.SquidPony.SquidBasicDemo;
+
+/** Launches the GWT application. */
+public class GwtLauncher extends GwtApplication {
+    @Override
+    public GwtApplicationConfiguration getConfig() {
+        GwtApplicationConfiguration configuration = new GwtApplicationConfiguration(80 * 11, (24 + 8) * 22);
+        return configuration;
+    }
+
+    @Override
+    public ApplicationListener createApplicationListener() {
+        return new SquidBasicDemo();
+    }
+}
