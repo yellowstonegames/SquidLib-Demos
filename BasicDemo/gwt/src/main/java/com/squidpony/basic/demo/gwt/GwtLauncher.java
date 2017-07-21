fe91d178c9155529239d44a4eeef9823a1cae690
Tommy Ettinger <tommy.ettinger@gmail.com>
2017-07-20 23:00:24 -0700

    Update BasicDemo with newer code, nicer features

---
+++ BasicDemo/gwt/src/main/java/com/squidpony/basic/demo/gwt/GwtLauncher.java
@@ -0,0 +1,20 @@
+package com.squidpony.basic.demo.gwt;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.squidpony.basic.demo.MainApplication;
+
+/** Launches the GWT application. */
+public class GwtLauncher extends GwtApplication {
+    @Override
+    public GwtApplicationConfiguration getConfig() {
+        GwtApplicationConfiguration configuration = new GwtApplicationConfiguration(640, 480);
+        return configuration;
+    }
+
+    @Override
+    public ApplicationListener createApplicationListener() {
+        return new MainApplication();
+    }
+}
