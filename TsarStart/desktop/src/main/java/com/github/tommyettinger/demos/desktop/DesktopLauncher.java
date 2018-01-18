397b8aedf2fa002ada8a557ef45cda795ec33592
Tommy Ettinger <tommy.ettinger@gmail.com>
2018-01-18 00:10:11 -0800

    Revise TheTsar with TsarStart; include GWT preview

---
+++ TsarStart/desktop/src/main/java/com/github/tommyettinger/demos/desktop/DesktopLauncher.java
@@ -0,0 +1,28 @@
+package com.github.tommyettinger.demos.desktop;
+
+import com.badlogic.gdx.Files.FileType;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.github.tommyettinger.demos.TsarDemo;
+
+/** Launches the desktop (LWJGL) application. */
+public class DesktopLauncher {
+    public static void main(String[] args) {
+        createApplication();
+    }
+
+    private static LwjglApplication createApplication() {
+        return new LwjglApplication(new TsarDemo(), getDefaultConfiguration());
+    }
+
+    private static LwjglApplicationConfiguration getDefaultConfiguration() {
+        LwjglApplicationConfiguration configuration = new LwjglApplicationConfiguration();
+        configuration.title = "TsarStart";
+        configuration.width = 90 * 10;
+        configuration.height = (25 + 7) * 20;
+        for (int size : new int[] { 128, 64, 32, 16 }) {
+            configuration.addIcon("libgdx" + size + ".png", FileType.Internal);
+        }
+        return configuration;
+    }
+}
