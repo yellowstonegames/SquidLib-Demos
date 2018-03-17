c4142e5b73c94e8c024d33e69175d83f82471d21
Tommy Ettinger <tommy.ettinger@gmail.com>
2018-03-17 16:55:16 -0700

    Speedup with Zug32RNG; waiting on quality tests
    Zug32RNG is able to omit one of the six xorshift operations that
    Zag32RNG uses by switching to an LCG instead of an LCG and adjusting the
    shift amounts. This seems to show a small speed boost, approaching
    ThrustAlt32RNG on nextInt() and exceeding anything else tested on
    nextLong(). Quality tests are still going and should be for at least a
    day more if they result in a successful pass; 8TB have been passed so
    far and what had been rough areas on earlier tests have gone smoothly
    this time, with two "unusual" anomalies and nothing worse than that.

--- Benchmarks/GWT/html/src/main/java/com/github/tommyettinger/bench/gwt/GwtLauncher.java
+++ Benchmarks/GWT/html/src/main/java/com/github/tommyettinger/bench/gwt/GwtLauncher.java
@@ -36,11 +36,13 @@ public class GwtLauncher extends GwtBareApp {
         vp.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_CENTER);
         addIntTest(vp, new ThrustAlt32RNG(123456789), "ThrustAlt32RNG");
         addIntTest(vp, new Zag32RNG(123456789, 987654321), "Zag32RNG");
+        addIntTest(vp, new Zug32RNG(123456789, 987654321), "Zug32RNG");
         addIntTest(vp, new LightRNG(123456L), "LightRNG");
         addIntTest(vp, new XoRoRNG(987654321L, 123456789L), "XoRoRNG");
         addIntTest(vp, new RandomXS128(123456789L, 987654321L), "RandomXS128");
         addLongTest(vp, new ThrustAlt32RNG(123456789), "ThrustAlt32RNG");
         addLongTest(vp, new Zag32RNG(123456789, 987654321), "Zag32RNG");
+        addLongTest(vp, new Zug32RNG(123456789, 987654321), "Zug32RNG");
         addLongTest(vp, new LightRNG(123456L), "LightRNG");
         addLongTest(vp, new XoRoRNG(987654321L, 123456789L), "XoRoRNG");
         addLongTest(vp, new RandomXS128(123456789L, 987654321L), "RandomXS128");
@@ -64,21 +66,27 @@ public class GwtLauncher extends GwtBareApp {
         Zag32RNG b = new Zag32RNG(123456789, 987654321);
         LightRNG c = new LightRNG(123456L);
         RandomXS128 d = new RandomXS128(123456789L, 987654321L);
+        Zug32RNG e = new Zug32RNG(123456789, 987654321);
 
         System.out.println("ThrustAlt32RNG " + a.nextInt());
         System.out.println("Zag32RNG " + b.nextInt());
         System.out.println("LightRNG " + c.nextInt());
         System.out.println("RandomXS128 " + d.nextInt());
+        System.out.println("Zug32RNG " + e.nextInt());
 
         System.out.println("ThrustAlt32RNG " + a.nextInt());
         System.out.println("Zag32RNG " + b.nextInt());
         System.out.println("LightRNG " + c.nextInt());
         System.out.println("RandomXS128 " + d.nextInt());
+        System.out.println("Zug32RNG " + e.nextInt());
 
         System.out.println("ThrustAlt32RNG " + a.nextInt());
         System.out.println("Zag32RNG " + b.nextInt());
         System.out.println("LightRNG " + c.nextInt());
         System.out.println("RandomXS128 " + d.nextInt());
+        System.out.println("Zug32RNG " + e.nextInt());
+
+        
     }
 
     private void addIntTest(final VerticalPanel vp, final ThrustAlt32RNG rs, final String name)
@@ -634,6 +642,109 @@ public class GwtLauncher extends GwtBareApp {
     }
 
     private int runLong(XoRoRNG rs) {
+        int xor = 0;
+        for (int i = 0; i < 10000; i++) {
+            xor ^= rs.nextLong();
+        }
+        return xor;
+    }
+
+    private void addIntTest(final VerticalPanel vp, final Zug32RNG rs, final String name)
+    {
+        final PushButton runBenchButton = new PushButton(name + ".nextInt(), " + rs.nextInt() + ", " + rs.nextInt() + ", " + rs.nextInt());
+        final TextBox resultLabel = new TextBox();
+        resultLabel.setReadOnly(false);
+        resultLabel.setText("Not run yet");
+        resultLabel.setPixelSize(560, 22);
+        runBenchButton.addClickHandler(new ClickHandler() {
+            public void onClick(ClickEvent event) {
+                resultLabel.setText("Running ...");
+                runBenchButton.setEnabled(false);
+
+                String benchmarkResult = reportInt(rs);
+
+                resultLabel.setText(benchmarkResult);
+                runBenchButton.setEnabled(true);
+            }
+        });
+        runBenchButton.setEnabled(true);
+        vp.add(runBenchButton);
+        vp.add(resultLabel);
+    }
+
+    private void addLongTest(final VerticalPanel vp, final Zug32RNG rs, final String name)
+    {
+        final PushButton runBenchButton = new PushButton(name + ".nextLong()");
+        final TextBox resultLabel = new TextBox();
+        resultLabel.setReadOnly(false);
+        resultLabel.setText("Not run yet");
+        resultLabel.setPixelSize(560, 22);
+        runBenchButton.addClickHandler(new ClickHandler() {
+            public void onClick(ClickEvent event) {
+                resultLabel.setText("Running ...");
+                runBenchButton.setEnabled(false);
+
+                String benchmarkResult = reportLong(rs);
+
+                resultLabel.setText(benchmarkResult);
+                runBenchButton.setEnabled(true);
+            }
+        });
+        runBenchButton.setEnabled(true);
+        vp.add(runBenchButton);
+        vp.add(resultLabel);
+    }
+    private String runIntBenchmark(Zug32RNG rs, long timeMinimum, int runsMinimum) {
+        int runs = 0;
+        IntVLA samples = new IntVLA();
+        long startTime, endTime, stopTime;
+        stopTime = System.currentTimeMillis() + timeMinimum;
+        int res = 0;
+        do {
+            startTime = System.currentTimeMillis();
+            res ^= runInt(rs);
+            endTime = System.currentTimeMillis();
+            samples.add((int) (endTime - startTime));
+        } while (++runs < runsMinimum || endTime < stopTime);
+
+        return res + "; " + runs + " runs; " + meanAndSEM(samples);
+    }
+
+    private String runLongBenchmark(Zug32RNG rs, long timeMinimum, int runsMinimum) {
+        int runs = 0;
+        IntVLA samples = new IntVLA();
+        long startTime, endTime, stopTime;
+        stopTime = System.currentTimeMillis() + timeMinimum;
+        int res = 0;
+        do {
+            startTime = System.currentTimeMillis();
+            res ^= runLong(rs);
+            endTime = System.currentTimeMillis();
+            samples.add((int) (endTime - startTime));
+        } while (++runs < runsMinimum || endTime < stopTime);
+
+        return res + "; " + runs + " runs; " + meanAndSEM(samples);
+    }
+
+    private String reportInt(Zug32RNG rs) {
+        runIntBenchmark(rs, 100, 2); // warm up
+        return runIntBenchmark(rs,2000, 5);
+    }
+
+    private int runInt(Zug32RNG rs) {
+        int xor = 0;
+        for (int i = 0; i < 10000; i++) {
+            xor ^= rs.nextInt();
+        }
+        return xor;
+    }
+
+    private String reportLong(Zug32RNG rs) {
+        runLongBenchmark(rs, 100, 2); // warm up
+        return runLongBenchmark(rs,2000, 5);
+    }
+
+    private int runLong(Zug32RNG rs) {
         int xor = 0;
         for (int i = 0; i < 10000; i++) {
             xor ^= rs.nextLong();
