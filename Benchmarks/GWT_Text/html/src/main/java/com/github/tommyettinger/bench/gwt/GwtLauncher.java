cba833a42f979452b2e953e6e7fddc820037c96b
Tommy Ettinger <tommy.ettinger@gmail.com>
2019-01-07 21:46:47 -0800

    Make the GWT text benchmark runnable from the web

--- Benchmarks/GWT_Text/html/src/main/java/com/github/tommyettinger/bench/gwt/GwtLauncher.java
+++ Benchmarks/GWT_Text/html/src/main/java/com/github/tommyettinger/bench/gwt/GwtLauncher.java
@@ -1,4 +1,6 @@
 package com.github.tommyettinger.bench.gwt;
+import squidpony.NaturalLanguageCipher;
+import squidpony.Thesaurus;
 import squidpony.annotation.GwtIncompatible;
 import com.badlogic.gdx.backends.headless.HeadlessApplication;
 import com.badlogic.gdx.ApplicationAdapter;
@@ -29,13 +31,16 @@ public class GwtLauncher extends GwtBareApp {
     }
 
 
-    public static final GWTRNG2 srng = new GWTRNG2(123L);
+    public static final GWTRNG srng = new GWTRNG(123L);
+    public static final Thesaurus thesaurus = new Thesaurus(123L);
+
     /**
      * This is the entry point method.
      */
     @Override
     public void start() {
 //        Gdx.app.setLogLevel(LOG_INFO);
+        thesaurus.addKnownCategories();
         VerticalPanel vp = new VerticalPanel();
         vp.setPixelSize(600, 780);
         vp.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_CENTER);
@@ -45,20 +50,15 @@ public class GwtLauncher extends GwtBareApp {
         vp.add(new Label(" * the time it took in milliseconds to generate those 100 sentences,"));
         vp.add(new Label(" * the margin of error for the calculation, in ms as an offset from the time before it."));
 
-        addLangTest(vp, FakeLanguageGen.SIMPLISH, "Simplified English");
-        addLangTest(vp, FakeLanguageGen.GREEK_AUTHENTIC, "Greek (Greek Script)");
-        addLangTest(vp, FakeLanguageGen.HINDI_ROMANIZED, "Hindi (Latin Script)");
-
+        addLangTest(vp, FakeLanguageGen.SIMPLISH, "Fake Generated English");
+        addLangTest(vp, FakeLanguageGen.GREEK_AUTHENTIC, "Fake Generated Greek ");
+        addLangTest(vp, FakeLanguageGen.HINDI_ROMANIZED, "Fake Generated Hindi");
+        addCipherTest(vp, new NaturalLanguageCipher(FakeLanguageGen.SIMPLISH), "Cipher to Fake English");
+        
         RootPanel.get().sinkEvents(Event.ONCLICK);
         RootPanel.get("embed-html").add(vp);
     }
 
-    /**
-     * Can be used to compare the three ints produced for each generator's nextInt() button with what a desktop JDK will
-     * produce given the same seeds. Light32RNG doesn't produce consistent results between GWT and desktop, so it isn't
-     * included in the GWT build (it's slower than Lathe32RNG anyway).
-     * @param args disregarded
-     */
     @GwtIncompatible
     public static void main(String[] args)
     {
@@ -72,6 +72,12 @@ public class GwtLauncher extends GwtBareApp {
                 System.out.println(FakeLanguageGen.GREEK_AUTHENTIC.sentence(srng, 3, 6));
                 srng.setState(123L);
                 System.out.println(FakeLanguageGen.HINDI_ROMANIZED.sentence(srng, 3, 6));
+                srng.setState(123L);
+                NaturalLanguageCipher gen = new NaturalLanguageCipher(FakeLanguageGen.SIMPLISH);
+                gen.setCacheLevel(0);
+                thesaurus.addKnownCategories();
+                String nation = thesaurus.makeNationName(gen.language);
+                System.out.println("nation: " + nation + ", ciphered: " + gen.cipher(nation));
                 Gdx.app.exit();
             }
         });
@@ -162,4 +168,65 @@ public class GwtLauncher extends GwtBareApp {
         }
         return xor;
     }
+
+
+    private void addCipherTest(final VerticalPanel vp, final NaturalLanguageCipher gen, final String name)
+    {
+        srng.setState(123L);
+        gen.setCacheLevel(0);
+        String nation = thesaurus.makeNationName(gen.language);
+        final PushButton runBenchButton = new PushButton(name + " nation: " + nation + ", ciphered: " + gen.cipher(nation));
+        final TextBox resultLabel = new TextBox();
+        resultLabel.setReadOnly(false);
+        resultLabel.setText("Not run yet");
+        resultLabel.setPixelSize(560, 22);
+        runBenchButton.addClickHandler(new ClickHandler() {
+            public void onClick(ClickEvent event) {
+                resultLabel.setText("Running ...");
+                runBenchButton.setEnabled(false);
+
+                String benchmarkResult = reportCipher(gen);
+
+                resultLabel.setText(benchmarkResult);
+                runBenchButton.setEnabled(true);
+            }
+        });
+        runBenchButton.setEnabled(true);
+        vp.add(runBenchButton);
+        vp.add(resultLabel);
+    }
+
+    /** Run the benchmark the specified number of milliseconds and return
+     *  the mean execution time and SEM in milliseconds.
+     */
+    private String runCipherBenchmark(NaturalLanguageCipher gen, long timeMinimum, int runsMinimum) {
+        int runs = 0;
+        IntVLA samples = new IntVLA();
+        long startTime, endTime, stopTime;
+        stopTime = System.currentTimeMillis() + timeMinimum;
+        int res = 0;
+        do {
+            startTime = System.currentTimeMillis();
+            res ^= runCipher(gen);
+            endTime = System.currentTimeMillis();
+            samples.add((int) (endTime - startTime));
+        } while (++runs < runsMinimum || endTime < stopTime);
+
+        return res + "; " + runs + " runs; " + meanAndSEM(samples);
+    }
+
+    private String reportCipher(NaturalLanguageCipher gen) {
+        srng.setState(1234567890L);
+        runCipherBenchmark(gen, 100, 2); // warm up
+        return runCipherBenchmark(gen,2000, 5);
+    }
+
+    private int runCipher(NaturalLanguageCipher gen) {
+        int xor = 0;
+        for (int i = 0; i < 100; i++) {
+            xor ^= gen.lookup(srng.getRandomElement(Thesaurus.noun.randomValue(srng))).hashCode();
+        }
+        return xor;
+    }
+
 }
