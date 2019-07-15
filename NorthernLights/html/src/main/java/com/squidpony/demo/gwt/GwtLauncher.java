bf6c2cd60b576dbbb2b569bf56d9cfc057128738
Tommy Ettinger <tommy.ettinger@gmail.com>
2019-07-14 21:10:53 -0700

    Set up NorthernLights for GWT again, should be in NorthernLights2

--- NorthernLights/html/src/main/java/com/squidpony/demo/gwt/GwtLauncher.java
+++ NorthernLights/html/src/main/java/com/squidpony/demo/gwt/GwtLauncher.java
@@ -3,7 +3,7 @@ package com.squidpony.demo.gwt;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
-import com.squidpony.shader.NorthernLights;
+import com.squidpony.demo.NorthernLights;
 
 /** Launches the GWT application. */
 public class GwtLauncher extends GwtApplication {
