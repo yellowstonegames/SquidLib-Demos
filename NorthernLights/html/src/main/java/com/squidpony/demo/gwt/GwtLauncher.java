7e10f9c916acca7a27fe395b0f96a848290f8392
Tommy Ettinger <tommy.ettinger@gmail.com>
2019-07-28 08:37:35 -0700

    Make NorthernLights GWT version resizable; works with Electron

--- NorthernLights/html/src/main/java/com/squidpony/demo/gwt/GwtLauncher.java
+++ NorthernLights/html/src/main/java/com/squidpony/demo/gwt/GwtLauncher.java
@@ -1,16 +1,42 @@
 package com.squidpony.demo.gwt;
 
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.google.gwt.event.logical.shared.ResizeEvent;
+import com.google.gwt.event.logical.shared.ResizeHandler;
+import com.google.gwt.user.client.Window;
 import com.squidpony.demo.NorthernLights;
 
 /** Launches the GWT application. */
 public class GwtLauncher extends GwtApplication {
+    private static final int PADDING = 0;
+    private GwtApplicationConfiguration cfg;
+
     @Override
     public GwtApplicationConfiguration getConfig() {
-        GwtApplicationConfiguration configuration = new GwtApplicationConfiguration(960, 640);
-        return configuration;
+        int w = Window.getClientWidth() - PADDING;
+        int h = Window.getClientHeight() - PADDING;
+        cfg = new GwtApplicationConfiguration(w, h);
+        Window.enableScrolling(false);
+        Window.setMargin("0");
+        Window.addResizeHandler(new ResizeListener());
+        cfg.preferFlash = false;
+        return cfg;
+    }
+
+    class ResizeListener implements ResizeHandler {
+        @Override
+        public void onResize(ResizeEvent event) {
+            if (Gdx.graphics.isFullscreen()) return; // thanks MrStahlfege
+            int width = event.getWidth() - PADDING;
+            int height = event.getHeight() - PADDING;
+            getRootPanel().setWidth("" + width + "px");
+            getRootPanel().setHeight("" + height + "px");
+            getApplicationListener().resize(width, height);
+            Gdx.graphics.setWindowedMode(width, height);
+        }
     }
 
     @Override
