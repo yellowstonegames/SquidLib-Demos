a7f93d2c10f61eec99434965d8db708da73da41b
Tommy Ettinger <tommy.ettinger@gmail.com>
2019-07-30 04:43:10 -0700

    Minor changes to reduce pressure on GLSL float precision

--- docs/NorthernLightsTesting/assets/scrambler_fragment_no_dither.glsl
+++ docs/NorthernLightsTesting/assets/scrambler_fragment_no_dither.glsl
@@ -14,8 +14,8 @@ uniform float tm;
 float swayRandomized(float seed, float value)
 {
     float f = floor(value);
-    float start = sin((cos(f + seed) * 64.987654321 + seed) * 120.413);
-    float end   = sin((cos((f+1.0) + seed) * 64.987654321 + seed) * 120.413);
+    float start = sin((cos(f + seed) * 62.97531 + seed) * 100.413);
+    float end   = sin((cos((f+1.0) + seed) * 62.97531 + seed) * 100.413);
     return mix(start, end, smoothstep(0.0, 1.0, value - f));
 }
 vec3 cosmic(vec3 seed, vec3 con)
@@ -29,7 +29,7 @@ vec3 cosmic(vec3 seed, vec3 con)
 void main() {
   vec3 alt = vec3(gl_FragCoord.xy, tm) * 0.0075;
   vec3 con = alt.yzx + alt.zxy;
-  vec3 s = 5555.555 + 2525.25 * fract(vec3(seed * 0.6180339887498949, seed * 0.7548776662466927, seed * 0.5698402909980532));
+  vec3 s = 12121.555 + 2525.25 * fract(vec3(seed * 0.61803, seed * 0.754877666, seed * 0.56984));
 
   con += cosmic(s.yzx, con.xyz) * 1.6;
   con += cosmic(s.zxy, con.yzx) * 1.6;
@@ -44,6 +44,6 @@ void main() {
 //  con += cosmic(s.zxy, con.yzx);
 //  con += cosmic(s.xyz, con.zxy);
     
-  gl_FragColor.rgb = cos(con * 3.14159265) * 0.5 + 0.5;
+  gl_FragColor.rgb = cos(con * 3.14159) * 0.5 + 0.5;
   gl_FragColor.a = v_color.a;
 }
