453cd4e964c83b55afdd8e6fbee9b08cfc5c9c41
Tommy Ettinger <tommy.ettinger@gmail.com>
2020-12-04 17:33:42 -0800

    Experiment with full-screen, alt-tab in LWJGL3

--- DawnlikeDemo/lwjgl3/src/main/java/com/squidpony/lwjgl3/Lwjgl3Launcher.java
+++ DawnlikeDemo/lwjgl3/src/main/java/com/squidpony/lwjgl3/Lwjgl3Launcher.java
@@ -1,7 +1,8 @@
 package com.squidpony.lwjgl3;
 
-import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
-import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Graphics;
+import com.badlogic.gdx.backends.lwjgl3.*;
 import com.badlogic.gdx.graphics.glutils.ShaderProgram;
 import com.squidpony.DawnlikeDemo;
 
@@ -21,6 +22,44 @@ public class Lwjgl3Launcher {
         Lwjgl3ApplicationConfiguration configuration = new Lwjgl3ApplicationConfiguration();
         configuration.setTitle("DawnlikeDemo");
         configuration.setResizable(true);
+        configuration.useVsync(false);
+        configuration.setForegroundFPS(60);
+        configuration.setWindowListener(new Lwjgl3WindowAdapter() {
+            @Override
+            public void iconified(boolean isIconified) {
+                if(isIconified)
+                    Gdx.app.getApplicationListener().pause();
+                else {
+                    Gdx.app.getApplicationListener().resume();
+                }
+            }
+
+            @Override
+            public void maximized(boolean isMaximized) {
+                if(isMaximized)
+                    Gdx.graphics.setFullscreenMode(Gdx.graphics.getDisplayMode());
+                else
+                    Gdx.graphics.setWindowedMode(gridWidth * cellWidth, gridHeight * cellHeight);
+            }
+
+            @Override
+            public void focusLost() {
+                Gdx.app.postRunnable(() -> maximized(false));
+            }
+
+            @Override
+            public void focusGained() {
+//                Gdx.app.postRunnable(() -> maximized(true));
+            }
+
+            @Override
+            public boolean closeRequested() {
+                Gdx.app.exit();
+                return true;
+            }
+        });
+//        configuration.setDecorated(false);
+//        configuration.setAutoIconify(true);
         configuration.enableGLDebugOutput(true, System.out);
         ShaderProgram.prependVertexCode = "#version 110\n";
         ShaderProgram.prependFragmentCode = "#version 110\n";
