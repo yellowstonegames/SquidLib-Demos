910946a526996775ad8878fa3f3dcccb4071c0f8
Tommy Ettinger <tommy.ettinger@gmail.com>
2021-09-16 21:03:30 -0700

    Adjust Thule vertex shader. HTML build.

--- docs/thule/assets/warble_fragment_no_dither-03b32b41e9dc044ca60ba84a180161bf.glsl
+++ docs/thule/assets/warble_fragment_no_dither-03b32b41e9dc044ca60ba84a180161bf.glsl
@@ -7,9 +7,6 @@ precision highp float;
 
 varying LOWP vec4 v_color;
 varying vec2 v_texCoords;
-varying float v_time;
-varying vec3 v_s;
-varying vec3 v_c;
 uniform sampler2D u_texture;
 
 uniform float u_seed;
@@ -25,10 +22,10 @@ vec3 swayRandomized(vec3 seed, vec3 value)
 // even though it should always return a vec3 with components between -1 and 1, we use it carefully.
 vec3 cosmic(vec3 c, vec3 con)
 {
-    return (con
-    + swayRandomized(c, con.yzx)
-    + swayRandomized(c + 1.1, con.zxy)
-    + swayRandomized(c + 2.2, con.xyz)) * 0.25;
+    con += swayRandomized(c, con.yzx);
+    con += swayRandomized(c + 1.0, con.zxy);
+    con += swayRandomized(c + 2.0, con.xyz);
+    return con * 0.25;
 }
 
 void main()
@@ -36,11 +33,16 @@ void main()
     vec3 COEFFS = fract((u_seed + 23.4567) * vec3(0.8191725133961645, 0.6710436067037893, 0.5497004779019703)) + 0.5;
     // Normalized pixel coordinates (from 0 to 1)
     vec2 uv = gl_FragCoord.xy * 0.0625 + swayRandomized(COEFFS.zxy, (u_time * 0.1875) * COEFFS.yzx).xy * 32.0;
-
-    vec3 con = vec3(0.0004375, 0.0005625, 0.0008125) * v_time + v_c * uv.x + v_s * uv.y;
+    // aTime, s, and c could be uniforms in some engines.
+    float aTime = u_time * 0.05;
+    vec3 adj = vec3(-1.11, 1.41, 1.61);
+    vec3 s = (swayRandomized(vec3(34.0, 76.0, 59.0), aTime + adj)) * 0.25;
+    vec3 c = (swayRandomized(vec3(27.0, 67.0, 45.0), aTime - adj)) * 0.25;
+    vec3 con = vec3(0.0004375, 0.0005625, 0.0008125) * aTime + c * uv.x + s * uv.y;
 
     con = cosmic(COEFFS, con);
-    con = cosmic(COEFFS + 1.618, con + COEFFS);
+    con = cosmic(COEFFS, con);
+    con = cosmic(COEFFS, con);
 
-    gl_FragColor = vec4(swayRandomized(COEFFS + 3.0, con * 3.14159265) * 0.5 + 0.5, 1.0);
+    gl_FragColor = vec4(swayRandomized(COEFFS + 3.0, con * (3.14159265)) * 0.5 + 0.5,1.0);
 }
