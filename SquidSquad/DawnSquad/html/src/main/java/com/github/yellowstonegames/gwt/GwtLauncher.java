419201fb818dd9f3d1e5e4e59b70a3fec457fe57
Tommy Ettinger <tommy.ettinger@gmail.com>
2021-12-10 15:56:04 -0800

    Creating a new DawnSquad project builds fine... Huh.

---
+++ SquidSquad/DawnSquad/html/src/main/java/com/github/yellowstonegames/gwt/GwtLauncher.java
@@ -0,0 +1,31 @@
+package com.github.yellowstonegames.gwt;
+
+import static com.github.tommyettinger.DawnSquad.cellHeight;
+import static com.github.tommyettinger.DawnSquad.cellWidth;
+import static com.github.tommyettinger.DawnSquad.gridHeight;
+import static com.github.tommyettinger.DawnSquad.gridWidth;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.github.tommyettinger.DawnSquad;
+
+/** Launches the GWT application. */
+public class GwtLauncher extends GwtApplication {
+		@Override
+		public GwtApplicationConfiguration getConfig () {
+			// Resizable application, uses available space in browser with no padding:
+//			GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(true);
+//			cfg.padVertical = 0;
+//			cfg.padHorizontal = 0;
+//			return cfg;
+			// If you want a fixed size application, comment out the above resizable section,
+			// and uncomment below:
+			return new GwtApplicationConfiguration(gridWidth * cellWidth * 2, gridHeight * cellHeight * 2);
+		}
+
+		@Override
+		public ApplicationListener createApplicationListener () {
+			return new DawnSquad();
+		}
+}
