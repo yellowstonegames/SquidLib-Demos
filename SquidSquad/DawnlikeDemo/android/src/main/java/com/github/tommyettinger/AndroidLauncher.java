09d7291815e8bd2e3f1c6dc64d7a67c1d43fd84e
Tommy Ettinger <tommy.ettinger@gmail.com>
2021-12-10 15:55:40 -0800

    Still trying to figure out what's wrong with SquidSquad/DawnlikeDemo...

--- SquidSquad/DawnlikeDemo/android/src/main/java/com/github/tommyettinger/AndroidLauncher.java
+++ SquidSquad/DawnlikeDemo/android/src/main/java/com/github/tommyettinger/AndroidLauncher.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.github.tommyettinger.DawnlikeDemo;
 
 /** Launches the Android application. */
 public class AndroidLauncher extends AndroidApplication {
