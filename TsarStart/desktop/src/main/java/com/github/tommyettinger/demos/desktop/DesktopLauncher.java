5ad1f78d98bae3207c58e4d7b43f17ad53359546
Tommy Ettinger <tommy.ettinger@gmail.com>
2021-12-13 22:16:10 -0800

    Get TsarStart working on LWJGL3 and generally updated.

--- TsarStart/desktop/src/main/java/com/github/tommyettinger/demos/desktop/DesktopLauncher.java
+++ TsarStart/desktop/src/main/java/com/github/tommyettinger/demos/desktop/DesktopLauncher.java
@@ -1,33 +1,28 @@
 package com.github.tommyettinger.demos.desktop;
 
-import com.badlogic.gdx.Files.FileType;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.github.tommyettinger.demos.TsarDemo;
 
 import static com.github.tommyettinger.demos.TsarDemo.*;
 
-/** Launches the desktop (LWJGL) application. */
+/** Launches the desktop (LWJGL3) application. */
 public class DesktopLauncher {
     public static void main(String[] args) {
         createApplication();
     }
 
-    private static LwjglApplication createApplication() {
-        return new LwjglApplication(new TsarDemo(), getDefaultConfiguration());
+    private static Lwjgl3Application createApplication() {
+        return new Lwjgl3Application(new TsarDemo(), getDefaultConfiguration());
     }
 
-    private static LwjglApplicationConfiguration getDefaultConfiguration() {
-        LwjglApplicationConfiguration configuration = new LwjglApplicationConfiguration();
-        configuration.title = "TsarStart";
-        configuration.width = gridWidth * cellWidth;
-        configuration.height = (gridHeight + bonusHeight) * cellHeight;
-        configuration.vSyncEnabled = true;
-        configuration.forceExit = false;
-        configuration.foregroundFPS = 0;
-        for (int size : new int[] { 128, 64, 32, 16 }) {
-            configuration.addIcon("libgdx" + size + ".png", FileType.Internal);
-        }
+    private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
+        Lwjgl3ApplicationConfiguration configuration = new Lwjgl3ApplicationConfiguration();
+        configuration.setTitle("TsarStart");
+        configuration.setWindowedMode(gridWidth * cellWidth, (gridHeight + bonusHeight) * cellHeight);
+        configuration.useVsync(true);
+        configuration.setForegroundFPS(300);
+        configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
         return configuration;
     }
 }
