314cf472da4ce4905f6e482765325c4ab1a2f6c5
Tommy Ettinger <tommy.ettinger@gmail.com>
2021-12-17 23:47:09 -0800

    Major update to KeyRemapper; Gradle 7.3.2, optional ProGuard/jpackage.

--- KeyRemapper/core/src/main/java/com/github/yellowstonegames/KeyRemap.java
+++ KeyRemapper/core/src/main/java/com/github/yellowstonegames/KeyRemap.java
@@ -20,17 +20,7 @@ import squidpony.squidmath.OrderedMap;
 import static squidpony.squidgrid.gui.gdx.SquidInput.*;
 
 /**
- * This is a small, not-overly-simple demo that presents some important features of SquidLib and shows a faster,
- * cleaner, and more recently-introduced way of displaying the map and other text. Features include dungeon map
- * generation, field of view, pathfinding (to the mouse position), continuous noise (used for a wavering torch effect),
- * language generation/ciphering, a colorful glow effect, and ever-present random number generation (with a seed).
- * You can increase the size of the map on most target platforms (but GWT struggles with large... anything) by
- * changing gridHeight and gridWidth to affect the visible area or bigWidth and bigHeight to adjust the size of the
- * dungeon you can move through, with the camera following your '@' symbol.
- * <br>
- * The assets folder of this project, if it was created with SquidSetup, will contain the necessary font files (just one
- * .fnt file and one .png are needed, but many more are included by default). You should move any font files you don't
- * use out of the assets directory when you produce a release JAR, APK, or GWT build.
+ * A utility that lets users build key remappings to fit their personal preferences or hardware requirements.
  */
 public class KeyRemap extends ApplicationAdapter {
     SpriteBatch batch;
