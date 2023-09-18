7bdd5ce196f19d297ce5df33a049df6ff5cb3d85
Tommy Ettinger <tommy.ettinger@gmail.com>
2023-09-17 23:46:05 -0700

    Show a much cleaner onModuleLoad().
    This is for gdx-freetype-gwt .

--- SquidSquad/DawnSquad/html/src/main/java/com/github/yellowstonegames/gwt/GwtLauncher.java
+++ SquidSquad/DawnSquad/html/src/main/java/com/github/yellowstonegames/gwt/GwtLauncher.java
@@ -1,33 +1,35 @@
 package com.github.yellowstonegames.gwt;
 
-import static com.github.tommyettinger.DawnSquad.cellHeight;
-import static com.github.tommyettinger.DawnSquad.cellWidth;
-import static com.github.tommyettinger.DawnSquad.gridHeight;
-import static com.github.tommyettinger.DawnSquad.gridWidth;
-
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
 import com.github.tommyettinger.DawnSquad;
 
+import static com.github.tommyettinger.DawnSquad.*;
+
 /** Launches the GWT application. */
 public class GwtLauncher extends GwtApplication {
-		@Override
-		public GwtApplicationConfiguration getConfig () {
-			// Resizable application, uses available space in browser with no padding:
+	@Override
+	public GwtApplicationConfiguration getConfig() {
+		// Resizable application, uses available space in browser with no padding:
 //			GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(true);
 //			cfg.padVertical = 0;
 //			cfg.padHorizontal = 0;
 //			return cfg;
-			// If you want a fixed size application, comment out the above resizable section,
-			// and uncomment below:
-			GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(gridWidth * cellWidth, gridHeight * cellHeight);
-			cfg.disableAudio = true;
-			return cfg;
-		}
+		// If you want a fixed size application, comment out the above resizable section,
+		// and uncomment below:
+		GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(gridWidth * cellWidth, gridHeight * cellHeight);
+		cfg.disableAudio = true;
+		return cfg;
+	}
 
-		@Override
-		public ApplicationListener createApplicationListener () {
-			return new DawnSquad();
-		}
+	@Override
+	public ApplicationListener createApplicationListener() {
+		return new DawnSquad();
+	}
+
+	@Override
+	public void onModuleLoad() {
+		com.badlogic.gdx.graphics.g2d.freetype.gwt.FreetypeInjector.inject(super::onModuleLoad);
+	}
 }
