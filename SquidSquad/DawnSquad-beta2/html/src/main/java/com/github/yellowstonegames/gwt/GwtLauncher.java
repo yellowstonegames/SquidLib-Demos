d799eaba752ad3fd2a8d50200fa0ffe0c90f9b9f
Tommy Ettinger <tommy.ettinger@gmail.com>
2024-06-15 20:50:04 -0700

    Add dawnsquad-b1 and -b2 to Pages.

--- SquidSquad/DawnSquad-beta2/html/src/main/java/com/github/yellowstonegames/gwt/GwtLauncher.java
+++ SquidSquad/DawnSquad-beta2/html/src/main/java/com/github/yellowstonegames/gwt/GwtLauncher.java
@@ -5,22 +5,27 @@ import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
 import com.github.yellowstonegames.SunriseSquad;
 
+import static com.github.yellowstonegames.SunriseSquad.*;
+
 /** Launches the GWT application. */
 public class GwtLauncher extends GwtApplication {
-        @Override
-        public GwtApplicationConfiguration getConfig () {
-            // Resizable application, uses available space in browser with no padding:
-            GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(true);
-            cfg.padVertical = 0;
-            cfg.padHorizontal = 0;
-            return cfg;
-            // If you want a fixed size application, comment out the above resizable section,
-            // and uncomment below:
-            //return new GwtApplicationConfiguration(640, 480);
-        }
+    @Override
+    public GwtApplicationConfiguration getConfig() {
+        // Resizable application, uses available space in browser with no padding:
+//			GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(true);
+//			cfg.padVertical = 0;
+//			cfg.padHorizontal = 0;
+//			return cfg;
+        // If you want a fixed size application, comment out the above resizable section,
+        // and uncomment below:
+        GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(shownWidth * cellWidth, shownHeight * cellHeight);
+        cfg.disableAudio = true;
+        return cfg;
+    }
 
-        @Override
-        public ApplicationListener createApplicationListener () {
-            return new SunriseSquad();
-        }
+    @Override
+    public ApplicationListener createApplicationListener() {
+//		return new SunriseSquad(0L);
+        return new SunriseSquad(System.currentTimeMillis());
+    }
 }
