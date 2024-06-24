7159c038f8cbfb8e406a5e40acada441b75ad07b
Tommy Ettinger <tommy.ettinger@gmail.com>
2024-06-24 15:02:56 -0700

    Show one way to use Gdx.files from tests.

--- SquidSquad/DawnSquad-beta2/core/src/test/java/com/github/yellowstonegames/BasicTest.java
+++ SquidSquad/DawnSquad-beta2/core/src/test/java/com/github/yellowstonegames/BasicTest.java
@@ -1,5 +1,8 @@
 package com.github.yellowstonegames;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.backends.headless.HeadlessFiles;
+import com.badlogic.gdx.utils.GdxNativesLoader;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -9,5 +12,12 @@ public class BasicTest {
         int a = 10;
         int b = 5;
         Assert.assertEquals(a, b + b);
+    }
+
+    @Test
+    public void testFiles() {
+        GdxNativesLoader.load();
+        Gdx.files = new HeadlessFiles();
+        Assert.assertFalse(Gdx.files.local("foobarbaz.txt").exists());
     }
 }
