194cafd2d7b4d544c754330de50d0005e98dbd20
Tommy Ettinger <tommy.ettinger@gmail.com>
2024-11-06 21:05:31 -0800

    Split 1.13.0 and 1.12.1 versions up.
    With the false positives 1.13.0 triggers, it makes sense to have more support for 1.12.1. Spritz still works with 1.12.1 .

---
+++ SquidSquad/DawnSquad-1-13-0/android/src/main/java/com/github/yellowstonegames/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.github.yellowstonegames.android;
+
+import android.os.Bundle;
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.github.tommyettinger.SunriseSquad;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+		initialize(new SunriseSquad(System.currentTimeMillis()), configuration);
+	}
+}
---
+++ SquidSquad/DawnSquad-1-13-0/html/src/main/java/com/github/yellowstonegames/gwt/GwtLauncher.java
@@ -0,0 +1,36 @@
+package com.github.yellowstonegames.gwt;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.github.tommyettinger.SunriseSquad;
+
+import static com.github.tommyettinger.SunriseSquad.*;
+
+/** Launches the GWT application. */
+public class GwtLauncher extends GwtApplication {
+	@Override
+	public GwtApplicationConfiguration getConfig() {
+		// Resizable application, uses available space in browser with no padding:
+//			GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(true);
+//			cfg.padVertical = 0;
+//			cfg.padHorizontal = 0;
+//			return cfg;
+		// If you want a fixed size application, comment out the above resizable section,
+		// and uncomment below:
+		GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(shownWidth * cellWidth, shownHeight * cellHeight);
+		cfg.disableAudio = true;
+		return cfg;
+	}
+
+	@Override
+	public ApplicationListener createApplicationListener() {
+//		return new SunriseSquad(0L);
+		return new SunriseSquad(System.currentTimeMillis());
+	}
+//
+//	@Override
+//	public void onModuleLoad() {
+//		com.badlogic.gdx.graphics.g2d.freetype.gwt.FreetypeInjector.inject(super::onModuleLoad);
+//	}
+}
