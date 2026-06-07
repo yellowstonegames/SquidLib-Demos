83dc86de825be6bea26c72a8a022caaa6881b2ab
Tommy Ettinger <tommy.ettinger@gmail.com>
2026-06-06 20:29:17 -0700

    Update DungeonDemo2 quite a lot.

--- SquidSquad/DungeonDemo2/lwjgl3/src/main/java/com/github/yellowstonegames/lwjgl3/Lwjgl3Launcher.java
+++ SquidSquad/DungeonDemo2/lwjgl3/src/main/java/com/github/yellowstonegames/lwjgl3/Lwjgl3Launcher.java
@@ -35,13 +35,6 @@ public class Lwjgl3Launcher {
         //// They can also be loaded from the root of assets/ .
         configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
 
-        //// This should improve compatibility with Windows machines with buggy OpenGL drivers, Macs
-        //// with Apple Silicon that have to emulate compatibility with OpenGL anyway, and more.
-        //// This uses the dependency `com.badlogicgames.gdx:gdx-lwjgl3-angle` to function.
-        //// You can choose to remove the following line and the mentioned dependency if you want; they
-        //// are not intended for games that use GL30 (which is compatibility with OpenGL ES 3.0).
-        configuration.setOpenGLEmulation(Lwjgl3ApplicationConfiguration.GLEmulation.ANGLE_GLES20, 0, 0);
-
         return configuration;
     }
 }
