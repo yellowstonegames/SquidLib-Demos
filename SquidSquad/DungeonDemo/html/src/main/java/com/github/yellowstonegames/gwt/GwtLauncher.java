e9b417ea1971e177f6d509b50c72acd3fab6d748
Tommy Ettinger <tommy.ettinger@gmail.com>
2026-06-06 20:37:00 -0700

    Move folders around and delete ancient projects.
    They're still in Git, but they wouldn't build with the current versions, probably. Their replacements build and work fine.

--- SquidSquad/DungeonDemo/html/src/main/java/com/github/yellowstonegames/gwt/GwtLauncher.java
+++ SquidSquad/DungeonDemo/html/src/main/java/com/github/yellowstonegames/gwt/GwtLauncher.java
@@ -5,26 +5,24 @@ import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
 import com.github.yellowstonegames.DungeonDemo;
 
-import static com.github.yellowstonegames.DungeonDemo.*;
-import static com.github.yellowstonegames.DungeonDemo.CELL_HEIGHT;
-
 /** Launches the GWT application. */
 public class GwtLauncher extends GwtApplication {
-		@Override
-		public GwtApplicationConfiguration getConfig () {
-			// Resizable application, uses available space in browser with no padding:
-//			GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(true);
-//			cfg.padVertical = 0;
-//			cfg.padHorizontal = 0;
-//			return cfg;
-			// If you want a fixed size application, comment out the above resizable section,
-			// and uncomment below:
-			GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(PLACE_WIDTH * CELL_WIDTH, PLACE_HEIGHT * CELL_HEIGHT);
-			return cfg;
-		}
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+            // Resizable application, uses available space in browser with no padding:
+//            GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(true);
+//            cfg.padVertical = 0;
+//            cfg.padHorizontal = 0;
+//            return cfg;
+            // If you want a fixed size application, comment out the above resizable section,
+            // and uncomment below:
+            return new GwtApplicationConfiguration(
+                DungeonDemo.SHOWN_WIDTH * DungeonDemo.CELL_WIDTH,
+                DungeonDemo.SHOWN_HEIGHT * DungeonDemo.CELL_HEIGHT);
+        }
 
-		@Override
-		public ApplicationListener createApplicationListener () {
-			return new DungeonDemo();
-		}
+        @Override
+        public ApplicationListener createApplicationListener () {
+            return new DungeonDemo();
+        }
 }
